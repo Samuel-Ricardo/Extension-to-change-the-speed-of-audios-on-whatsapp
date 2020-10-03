1f54506d04e5abcd379bd82da495e78eb3a4e345
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-10-03 11:33:35 -0300

    added comments

--- Content.js
+++ Content.js
@@ -1,10 +1,10 @@
 
 
-const interval = setInterval(()=>{
+const interval = setInterval(()=>{  // checks every 1 second if the header has already been created // checa a cada 1 segundo se o header já foi criado
    
     const header = document.querySelector("._1QUKR")
 
-    if(header){
+    if(header){ // if it has already been created it stops repeating and creates the button and the Input // se já tiver sido criado ele para de repetir e cria o botao e o Input
 
         clearInterval(interval)
 
@@ -24,15 +24,16 @@ const interval = setInterval(()=>{
 
             const audios = document.querySelectorAll("audio")
 
-            audios.forEach((audio) => {
+            audios.forEach((audio) => { // scan the audios // varre os audios
 
                 console.log(audios)
-                audio.playbackRate = Number(numberInput.value);
+                audio.playbackRate = Number(numberInput.value); // change their speed with the input value // modifica a velocidade deles com o valor do input
             })
 
         })
 
-        
+         // put inside the header // põe dentro do header
+
         header.appendChild(numberInput)
         header.appendChild(button)
     }
